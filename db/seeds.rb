95fd2b541c76a08afcdcadcea3593600017b7fe4
Christian Perez <chris.dillon92@gmail.com>
2016-11-22 14:17:18 -0800

    adds basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: 'Andy', email: 'andy@email.com'}, {name: 'Brian', email: 'brian@email.com'}, {name: 'Candy', email: 'candy@email.com'}])
