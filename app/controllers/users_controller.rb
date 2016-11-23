345994f2ffc92f9fbf6fedfd6feb3b809022bd02
Christian Perez <chris.dillon92@gmail.com>
2016-11-22 16:47:46 -0800

    adds password & bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new(user_params)
   end
 
   def show
-    @user = User.first
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[user_params])
+    if @user.save
+      puts @user.name += " was created"
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # def update
+
+  # end
 end
