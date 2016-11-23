15367f3c7af1abaa3e7c8ab524906222766e0346
Christian Perez <chris.dillon92@gmail.com>
2016-11-23 14:45:35 -0800

    minimum working for chapter 10/11

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+    # if user
+      puts "logged in"
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+end
