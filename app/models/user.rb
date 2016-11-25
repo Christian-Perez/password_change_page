c18002ce09a4c6b94569a805c05ea7c228493b0d
Christian Perez <chris.dillon92@gmail.com>
2016-11-25 00:48:00 -0800

    reset working, not sending email with link to reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
   private
 
   def User.digest(string)
